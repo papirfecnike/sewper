eeff7c122adde57749fc8e037b057fd7543f809e
papirfecnike <papirfecnike@yahoo.com>
2020-04-19 22:47:37 +0200

    feat: use Material UI

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './input.css';
+import 'typeface-roboto';
+import Icon from '@material-ui/core/Icon';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
 
 function Input (props) {
-
-
     return (<label>{props.label}<input type="number" /></label>)
 }
 
