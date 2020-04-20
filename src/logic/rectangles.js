c108a3cf494172b3586b454025e0b334c17b3404
papirfecnike <papirfecnike@yahoo.com>
2020-04-20 23:03:47 +0200

    feat: parameter validation

--- src/logic/rectangles.js
+++ src/logic/rectangles.js
@@ -1,6 +1,13 @@
 // Torso = [Widest of (Chest width, Waist width, Hip width) + Seam width x 2] x (Full length + Seam width x 2)
 
-export function calculateTorso(chestWidth, waistWidth, hipWidth, seamWidth, fullLength) {
+export function calculateTorso(torsoParameters) {
+    const {
+        chestWidth,
+        waistWidth,
+        hipWidth,
+        fullLength,
+        seamWidth
+    } = torsoParameters
     const widestWidth = Math.max(chestWidth, waistWidth, hipWidth);
     const area = calculateArea(widestWidth, fullLength, seamWidth);
     return area;
@@ -8,20 +15,62 @@ export function calculateTorso(chestWidth, waistWidth, hipWidth, seamWidth, full
 
 // Arms = (Arm circumference + Seam width x 2) x (Arm length + Seam width x 2)
 
-export function calculateArm(armCircumference, seamWidth, armLength) {
+export function calculateArm(armParameters) {
+    const {
+        armCircumference,
+        armLength,
+        seamWidth
+    } = armParameters
     const area = calculateArea(armCircumference, armLength, seamWidth);
     return area;
 }
 
 // Neck = (Width + Seam width x 2) x (Length + Seam width x 2)
 
-export function calculateNeck(width, length, seamWidth) {
+export function calculateNeck(neckParameters) {
+    const {
+        width,
+        length,
+        seamWidth
+    } = neckParameters
     const area = calculateArea(width, length, seamWidth);
     return area;
 }
 
 // logic
 
-function calculateArea (width, length, seam) {
+export function calculateArea (width, length, seam) {
     return (width + (seam * 2)) * (length + (seam * 2));
+}
+
+// parameter validation
+
+export function validateTorsoParameters (torsoParameters) {
+    const torsoKeys = ["chestWidth", "waistWidth", "hipWidth", "fullLength", "seamWidth"]
+    Object.keys(torsoParameters).forEach(function (key) {
+        if (!torsoKeys.includes(key)) {
+            throw new Error ("Please type correct key. Parameter key " + key + " is not valid.")
+        }
+    })
+    return true;
+}
+
+export function validateArmParameters (armParameters) {
+    const armKeys = ["armCircumference", "armLength", "seamWidth"]
+    Object.keys(armParameters).forEach(function (key) {
+        if (!armKeys.includes(key)) {
+            throw new Error ("Please type correct key. Parameter key " + key + " is not valid.")
+        }
+    })
+    return true;
+}
+
+export function validateNeckParameters (neckParameters) {
+    const neckKeys = ["width", "length", "seamWidth"]
+    Object.keys(neckParameters).forEach(function (key) {
+        if (!neckKeys.includes(key)) {
+            throw new Error ("Please type correct key. Parameter key " + key + " is not valid.")
+        }
+    })
+    return true;
 }
--- src/logic/rectangles.test.js
+++ src/logic/rectangles.test.js
@@ -1,16 +1,64 @@
-import { calculateTorso, calculateArm, calculateNeck } from './rectangles.js';
+import { calculateTorso, calculateArm, calculateNeck, validateTorsoParameters, validateArmParameters, validateNeckParameters } from './rectangles.js';
 
 test('calculate Torso rectangles correctly', () => {
-    const rectangleArea = calculateTorso(60, 40, 80, 2, 90);
+    const torsoParameters = {
+        chestWidth: 60,
+        waistWidth: 40,
+        hipWidth: 80,
+        fullLength: 90,
+        seamWidth: 2
+    }
+    const rectangleArea = calculateTorso(torsoParameters);
     expect(rectangleArea).toBe(7896);
 });
 
 test('calculate Arm rectangles correctly', () => {
-    const rectangleArea = calculateArm(50, 2, 30);
+    const armParameters = {
+        armCircumference: 50,
+        armLength: 30,
+        seamWidth: 2
+    }
+    const rectangleArea = calculateArm(armParameters);
     expect(rectangleArea).toBe(1836);
 });
 
 test('calculate Neck rectangles correctly', () => {
-    const rectangleArea = calculateNeck(10, 40, 2);
+    const neckParameters = {
+        width: 10,
+        length: 40,
+        seamWidth: 2
+    }
+    const rectangleArea = calculateNeck(neckParameters);
     expect(rectangleArea).toBe(616);
 });
+
+test('validate Torso keys', () => {
+    const torsoParameters = {
+        chestWidth: 60,
+        waistWidth: 40,
+        hipWidth: 80,
+        fullLength: 90,
+        seamWidth: 2
+    }
+    expect(validateTorsoParameters(torsoParameters)).toBe(true);
+});
+
+test('validate Arm keys', () => {
+    const armParameters = {
+        armCircumference: 50,
+        armLength: 30,
+        seamWidth: 2
+    }
+    expect(validateArmParameters(armParameters)).toBe(true);
+});
+
+test('validate Neck keys', () => {
+    const neckParameters = {
+        width: 10,
+        length: 40,
+        seamWidth: 2
+    }
+    expect(validateNeckParameters(neckParameters)).toBe(true);
+});
+
+test.todo("calculateArea")
