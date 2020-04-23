cd898f52fa59916efdba338435e0354da40460d3
papirfecnike <53873408+papirfecnike@users.noreply.github.com>
2020-04-23 22:49:03 +0200

    chore: cleanup

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './input.css';
-import 'typeface-roboto';
 import Icon from '@material-ui/core/Icon';
 import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
