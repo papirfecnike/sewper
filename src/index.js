c346a43244a30246c4084e997168de21a17b7ab6
papirfecnike <53873408+papirfecnike@users.noreply.github.com>
2020-04-29 23:01:23 +0200

    MVP
    * feat: use Material UI
    
    * doc: logic
    
    * feat: calculate rectangles
    
    * feat: calculate area
    
    input validation first!
    
    * feat: material length calculation
    
    * feat: parameter validation
    
    * chore: update material-ui
    
    * feat: start using material-ui
    
    * test: calculateArea
    
    * chore: cleanup
    
    * chore: setup editorconfig
    
    * refactor: rename neck parameters
    
    * refactor: rename neck parameters
    
    * feat: turn values to numbers
    
    * feat: use logic to calculate
    
    * feat: finishing touches
    
    displaying calculation result for materialLength
    
    * feat: use numeric pattern on inputs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App.js';
+import theme from './theme.js';
+import * as serviceWorker from './serviceWorker.js';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>,
+    document.querySelector('#root'),
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/logic/area.test.js
@@ -0,0 +1,44 @@
+import { calculateMaterialLength, isValidParameter } from './area.js';
+
+test('calculate Material length', () => {
+    const torsoParameters = {
+        chestWidth: 60,
+        waistWidth: 40,
+        hipWidth: 80,
+        fullLength: 90,
+        seamWidth: 2
+    }
+    const armParameters = {
+        armCircumference: 30,
+        armLength: 20,
+        seamWidth: 2
+    }
+    const neckParameters = {
+        neckWidth: 40,
+        neckHeight: 10,
+        seamWidth: 2
+    }
+    const materialWidth = 150;
+
+    const materialLength = calculateMaterialLength(torsoParameters, armParameters, neckParameters, materialWidth);
+
+    expect(materialLength).toBe(120.26666666666667);
+});
+
+test('isValidParameter throws error if any of the values is not a number', () => {
+    const param = {
+        checkingvalue1: 24,
+        checkingvalue2: "string"
+    }
+    expect(() => {
+        isValidParameter(param);
+      }).toThrow();
+});
+
+test('isValidParameter returns true if all parameters are numbers', () => {
+    const param = {
+        checkingvalue1: 24,
+        checkingvalue2: 655,
+    }
+    expect(isValidParameter(param)).toBe(true);
+});
